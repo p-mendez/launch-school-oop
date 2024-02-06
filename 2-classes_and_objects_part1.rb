05a9aab6084fcdd34f7fd4fc9908044cf87c319e
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-02-05 22:25:02 -0500

    Add atr_accessor info

--- 2-classes_and_objects_part1.rb
+++ 2-classes_and_objects_part1.rb
@@ -91,24 +91,58 @@ using the same name as the instance variable they are exposing and setting.
 We'll make the change to our code as well:
 =end
 
+# class GoodDog
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def name
+#     @name
+#   end
+
+#   def name=(n)
+#     @name = n
+#   end
+
+#   def speak
+#     "#{@name} says Arf!"
+#   end
+# end
+
+# sparky = GoodDog.new("Sparky")
+# sparky.name = "Spartacus"
+# puts sparky.name
+
+
 class GoodDog
+  attr_accessor :name
+
   def initialize(name)
     @name = name
   end
 
-  def name
-    @name
-  end
-
-  def name=(n)
-    @name = n
-  end
-
   def speak
-    "#{@name} says Arf!"
+    "#{@name} says arf!"
   end
 end
 
 sparky = GoodDog.new("Sparky")
+puts sparky.speak
+puts sparky.name
 sparky.name = "Spartacus"
 puts sparky.name
+
+
+=begin
+The attr_accessor method takes a symbol as an argument, which it uses to create the method
+name for the getter and setter methods. That one line replaced two method definitions.
+
+
+But what if we only want the getter method without the setter method?
+Then we would want to use the attr_reader method. It works the same way but only allows you
+to retrieve the instance variable. And if you only want the setter method, you can use the
+attr_writer method. All of the attr_* methods take Symbol objects as arguments;
+
+if there are more states you're tracking, you can use this syntax:
+  attr_accessor :name, :height, :weight
+=end

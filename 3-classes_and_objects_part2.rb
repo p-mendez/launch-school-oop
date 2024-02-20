a979779a91e72a9fa6303575b323fb28dda693f8
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-02-19 22:13:59 -0500

    Add constant info

--- 3-classes_and_objects_part2.rb
+++ 3-classes_and_objects_part2.rb
@@ -15,10 +15,44 @@
 # Use @@ for class variables
 
 
+# class GoodDog
+#   @@number_of_dogs = 0
+
+#   def initialize
+#     @@number_of_dogs += 1
+#   end
+
+#   def self.total_number_of_dogs
+#     @@number_of_dogs
+#   end
+
+#   def self.what_am_i
+#     "I'm a GoodDog class!"
+#   end
+# end
+
+# puts GoodDog.total_number_of_dogs
+# dog1 = GoodDog.new
+# dog2 = GoodDog.new
+
+# puts GoodDog.total_number_of_dogs
+
+
+# _____________ CONSTANTS ____________
+# Use capital letter for constants. Convention is to use ALL_CAPS
+
 class GoodDog
+  # Constant
+  DOG_YEARS = 7
+
+  # Class variable
   @@number_of_dogs = 0
 
-  def initialize
+  attr_accessor :name, :age
+
+  def initialize(n, a)
+    self.name = n
+    self.age = a * DOG_YEARS
     @@number_of_dogs += 1
   end
 
@@ -31,8 +65,5 @@ class GoodDog
   end
 end
 
-puts GoodDog.total_number_of_dogs
-dog1 = GoodDog.new
-dog2 = GoodDog.new
-
-puts GoodDog.total_number_of_dogs
+sparky = GoodDog.new("Sparky", 4)
+puts sparky.age

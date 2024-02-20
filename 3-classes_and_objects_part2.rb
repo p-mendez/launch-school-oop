2fdfeef7bc09118c9bf2eeb4c2c2c357a6ba8a13
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-02-19 22:29:13 -0500

    Add to_s info

--- 3-classes_and_objects_part2.rb
+++ 3-classes_and_objects_part2.rb
@@ -41,29 +41,59 @@
 # _____________ CONSTANTS ____________
 # Use capital letter for constants. Convention is to use ALL_CAPS
 
+# class GoodDog
+#   # Constant
+#   DOG_YEARS = 7
+
+#   # Class variable
+#   @@number_of_dogs = 0
+
+#   attr_accessor :name, :age
+
+#   def initialize(n, a)
+#     self.name = n
+#     self.age = a * DOG_YEARS
+#     @@number_of_dogs += 1
+#   end
+
+#   def self.total_number_of_dogs
+#     @@number_of_dogs
+#   end
+
+#   def self.what_am_i
+#     "I'm a GoodDog class!"
+#   end
+# end
+
+# sparky = GoodDog.new("Sparky", 4)
+# puts sparky.age
+
+
+# _________ to_s  METHOD ________________
+# When printing an object the to_s method implicitly called
+# # In other words: puts sparky and puts sparky.to_s are equivalent.
+# For arrays, when printing the object it prints each element's to_s method
+
+
 class GoodDog
-  # Constant
-  DOG_YEARS = 7
-
-  # Class variable
-  @@number_of_dogs = 0
-
   attr_accessor :name, :age
 
   def initialize(n, a)
     self.name = n
-    self.age = a * DOG_YEARS
-    @@number_of_dogs += 1
+    self.age = a
   end
 
-  def self.total_number_of_dogs
-    @@number_of_dogs
-  end
-
-  def self.what_am_i
-    "I'm a GoodDog class!"
+  def to_s
+    "This dog's name is #{name} and it is #{age} years old."
   end
 end
 
-sparky = GoodDog.new("Sparky", 4)
-puts sparky.age
+sparky = GoodDog.new("Sparky", 28)
+puts sparky
+
+# p calls inspect method. Don't override that one!
+p sparky
+
+# to_s is what gets called for string interpolation.
+# If you don't override to_s then it uses the inherited to_s (Usually Object#to_s)
+# Make sure to_s always returns a string

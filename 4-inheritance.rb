9f30778f9af5e2d9f8645b005b3aa9dc0f4aae5d
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-02-23 13:17:08 -0500

    Add super keyword info

--- 4-inheritance.rb
+++ 4-inheritance.rb
@@ -1,30 +1,50 @@
-=begin
-  Inheritance, we use the < symbol to show that the subclass is inheriting from the superclass.
-=end
+# =begin
+#   Inheritance, we use the < symbol to show that the subclass is inheriting from the superclass.
+# =end
 
+
+# class Animal
+#   def speak
+#     "Hello!"
+#   end
+# end
+
+# class GoodDog < Animal
+#   attr_accessor :name
+
+#   def initialize(n)
+#     self.name = n
+#   end
+#   def speak                 # <- Overrides the inherited speak method
+#     "#{self.name} says Arf!"
+#   end
+# end
+
+# class Cat < Animal
+# end
+
+# sparky = GoodDog.new("Sparky")
+# paws = Cat.new
+
+# puts sparky.speak
+# puts paws.speak
+
+
+=begin
+  You can use super to access the superclass's method.
+=end
 
 class Animal
   def speak
-    "Hello!"
+    "Hello"
   end
 end
 
 class GoodDog < Animal
-  attr_accessor :name
-
-  def initialize(n)
-    self.name = n
-  end
-  def speak                 # <- Overrides the inherited speak method
-    "#{self.name} says Arf!"
+  def speak
+    super + ". Have a nice day!"
   end
 end
 
-class Cat < Animal
-end
-
-sparky = GoodDog.new("Sparky")
-paws = Cat.new
-
+sparky = GoodDog.new
 puts sparky.speak
-puts paws.speak

d2d62d2cf0a1172f19fb139b06a7bbb6fc18d5ed
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-03-04 22:32:25 -0500

    Add #start and #play_round to Game

--- projects/tic_tac_toe.rb
+++ projects/tic_tac_toe.rb
@@ -21,6 +21,11 @@ class Board
   def positions_available?
     board.value? nil
   end
+
+  def available_positions
+    p = board.select { |k, v| v.nil? }
+    p.keys.join(', ')   # comma separate the results
+  end
 end
 
 class Player
@@ -41,19 +46,40 @@ class Player
 end
 
 class Game
-  attr_reader :p1, :p2, :board
+  attr_reader :p1, :p2, :board, :current_player
 
   def initialize
     @p1 = Player.create_player_one
     @p2 = Player.create_player_two
     @board = Board.new
+    @current_player = self.p1
+  end
+
+  def start
+    while board.positions_available?
+      play_round
+    end
+  end
+
+  def play_round
+    player_prompt
+  end
+
+  def player_prompt
+    puts current_player
+    puts "Available positions: #{board.available_positions}"
+    print "Input your next move: "
+    next_move = gets.chomp
   end
 
 end
 
-b = Board.new
-p1 = Player.create_player_one
-p2 = Player.create_player_two
-puts b.positions_available?
-p p1
-p p2
+# b = Board.new
+# p1 = Player.create_player_one
+# p2 = Player.create_player_two
+# puts b.positions_available?
+# p p1
+# p p2
+
+game = Game.new
+game.start
